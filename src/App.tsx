aa1d73dc2b38fe3713a8b3e9784fe8c39555d162
Abdullah Ahmed <abnramdan2006@gmail.com>
2025-12-31 04:43:14 +0200

    update 0.10.10(beta)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, lazy, Suspense, useState } from "react";
+import { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -8,31 +8,30 @@ import Footer from "@/components/Footer";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import ScrollToHash from "./components/ScrollToHash";
 import ScrollToTop from "./components/ScrollToTop";
-import LoadingOverlay from "./components/LoadingOverlay";
 import { AnimatePresence, motion } from "framer-motion";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
 
-// Lazy Load Pages
-const Index = lazy(() => import("./pages/Index"));
-const SellingOptions = lazy(() => import("./pages/SellingOptions"));
-const WhoWeAre = lazy(() => import("./pages/WhoWeAre"));
-const Contact = lazy(() => import("./pages/Contact"));
-const HowItWorks = lazy(() => import("./pages/HowItWorks"));
-const Blog = lazy(() => import("./pages/blog"));
-const Testimonials = lazy(() => import("./pages/Testimonials"));
-const Partners = lazy(() => import("./pages/Partners"));
-const Agents = lazy(() => import("./pages/partners/Agents"));
-const Wholesalers = lazy(() => import("./pages/partners/Wholesalers"));
-const LocalInvestors = lazy(() => import("./pages/partners/LocalInvestors"));
-const TitleCompanies = lazy(() => import("./pages/partners/TitleCompanies"));
-const Faq = lazy(() => import("./pages/Faq"));
-const Careers = lazy(() => import("./pages/Careers"));
-const CityLandingPage = lazy(() => import("./pages/CityLandingPage"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
-const TermsOfService = lazy(() => import("./pages/TermsOfService"));
-const NotFound = lazy(() => import("./pages/NotFound"));
+// Static Imports (Removing Lazy Loading)
+import Index from "./pages/Index";
+import SellingOptions from "./pages/SellingOptions";
+import WhoWeAre from "./pages/WhoWeAre";
+import Contact from "./pages/Contact";
+import HowItWorks from "./pages/HowItWorks";
+import Blog from "./pages/blog";
+import Testimonials from "./pages/Testimonials";
+import Partners from "./pages/Partners";
+import Agents from "./pages/partners/Agents";
+import Wholesalers from "./pages/partners/Wholesalers";
+import LocalInvestors from "./pages/partners/LocalInvestors";
+import TitleCompanies from "./pages/partners/TitleCompanies";
+import Faq from "./pages/Faq";
+import Careers from "./pages/Careers";
+import CityLandingPage from "./pages/CityLandingPage";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -75,30 +74,28 @@ const AppContent = () => {
       <ScrollToHash />
 
       <Header />
-      <Suspense fallback={null}>
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<PageTransition><Index /></PageTransition>} />
-            <Route path="/selling-options" element={<PageTransition><SellingOptions /></PageTransition>} />
-            <Route path="/who-are-we" element={<PageTransition><WhoWeAre /></PageTransition>} />
-            <Route path="/contact" element={<PageTransition><Contact /></PageTransition>} />
-            <Route path="/how-it-works" element={<PageTransition><HowItWorks /></PageTransition>} />
-            <Route path="/blog" element={<PageTransition><Blog /></PageTransition>} />
-            <Route path="/testimonials" element={<PageTransition><Testimonials /></PageTransition>} />
-            <Route path="/partners" element={<PageTransition><Partners /></PageTransition>} />
-            <Route path="/partners/agents" element={<PageTransition><Agents /></PageTransition>} />
-            <Route path="/partners/wholesalers" element={<PageTransition><Wholesalers /></PageTransition>} />
-            <Route path="/partners/local-investors" element={<PageTransition><LocalInvestors /></PageTransition>} />
-            <Route path="/partners/title-companies" element={<PageTransition><TitleCompanies /></PageTransition>} />
-            <Route path="/faq" element={<PageTransition><Faq /></PageTransition>} />
-            <Route path="/careers" element={<PageTransition><Careers /></PageTransition>} />
-            <Route path="/we-buy-houses/:area" element={<PageTransition><CityLandingPage /></PageTransition>} />
-            <Route path="/privacy-policy" element={<PageTransition><PrivacyPolicy /></PageTransition>} />
-            <Route path="/terms-of-service" element={<PageTransition><TermsOfService /></PageTransition>} />
-            <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
-          </Routes>
-        </AnimatePresence>
-      </Suspense>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<PageTransition><Index /></PageTransition>} />
+          <Route path="/selling-options" element={<PageTransition><SellingOptions /></PageTransition>} />
+          <Route path="/who-are-we" element={<PageTransition><WhoWeAre /></PageTransition>} />
+          <Route path="/contact" element={<PageTransition><Contact /></PageTransition>} />
+          <Route path="/how-it-works" element={<PageTransition><HowItWorks /></PageTransition>} />
+          <Route path="/blog" element={<PageTransition><Blog /></PageTransition>} />
+          <Route path="/testimonials" element={<PageTransition><Testimonials /></PageTransition>} />
+          <Route path="/partners" element={<PageTransition><Partners /></PageTransition>} />
+          <Route path="/partners/agents" element={<PageTransition><Agents /></PageTransition>} />
+          <Route path="/partners/wholesalers" element={<PageTransition><Wholesalers /></PageTransition>} />
+          <Route path="/partners/local-investors" element={<PageTransition><LocalInvestors /></PageTransition>} />
+          <Route path="/partners/title-companies" element={<PageTransition><TitleCompanies /></PageTransition>} />
+          <Route path="/faq" element={<PageTransition><Faq /></PageTransition>} />
+          <Route path="/careers" element={<PageTransition><Careers /></PageTransition>} />
+          <Route path="/we-buy-houses/:area" element={<PageTransition><CityLandingPage /></PageTransition>} />
+          <Route path="/privacy-policy" element={<PageTransition><PrivacyPolicy /></PageTransition>} />
+          <Route path="/terms-of-service" element={<PageTransition><TermsOfService /></PageTransition>} />
+          <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
+        </Routes>
+      </AnimatePresence>
       <Footer />
     </TooltipProvider>
   );
